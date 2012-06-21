3bb2e0f62bf47a373ebc4e008656f4fe42002dad
solszak <Anwender@Ksw09.(none)>
2012-06-21 16:51:51 +0200

    initial commit

---
+++ src/program.js
@@ -0,0 +1,10 @@
+document.writeln("Hello World!\n");
+f(2.3, 4.5);
+
+function f(x, y)
+{
+	var pointd = new Object()
+	point.x = x;
+	point.y = y;
+	document.writeln(point.x);
+}
---
+++ src/server.js
@@ -0,0 +1,11 @@
+// A simple node server
+
+var http = require('http');
+
+http.createServer(function(request, response) {
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.end('<b>Hello World</b>');
+}).listen(8124);
+
+console.log('Server running at http://127.0.0.1:8124/');
+
