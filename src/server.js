904723452cddb7096e6ec038d2ac5d11570a428c
Raskolnikow <s.raskolnikow@gmail.com>
2012-06-22 13:51:52 +0200

    commit cleanup

--- src/server.js
+++ src/server.js
@@ -2,10 +2,34 @@
 
 var http = require('http');
 
-http.createServer(function(request, response) {
+/* --------------------------------------------------------------- */
+var server;
+
+/* --------------------------------------------------------------- */
+
+createServer();
+startServer(8124);
+
+/* --------------------------------------------------------------- */
+
+function createServer() {
+	server = http.createServer(requestFunc)
+}
+
+function startServer(port) {
+	server.listen(port);
+	console.log('Server running at http://127.0.0.1:8124/');
+}
+
+/*
+	Handles request from a client
+
+	@para request: http.ServerRequest
+	@para response: http.ServerResponse
+*/
+function requestFunc(request, response) {
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.end('<b>Hello World</b>');
-}).listen(8124);
-
-console.log('Server running at http://127.0.0.1:8124/');
+	console.log('Station connected!!\nMethod: ' + request.url)	
+}
 
