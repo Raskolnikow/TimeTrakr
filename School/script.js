37eca30d6e7f193e642622dcf52e3a0a06747b2c
Raskolnikow <s.raskolnikow@gmail.com>
2013-09-30 17:08:24 +0200

    New notes + playing with styles.

--- School/script.js
+++ School/script.js
@@ -1,20 +1,17 @@
 function output() {
+	var content = document.getElementById("content");
 	for (var i = 0; i < 10; i++) {
-		document.writeln("Number: " + i + "</br>");
+		content.innerHTML += "Number: " + i + "</br>";
 	};
 }
 
-function mani(s) {
-	s = new String("Bye");
+function ss() {
+	output();
 }
-
-var s = new String("Hello World");
-document.writeln(s + "</br></br>");
-mani(s);
-document.writeln(s+ "</br></br>");
+//var s = new String("Hello World from JavaScript");
+//document.writeln(s + "</br></br>");
 
 
-
-var now = new Date();
-document.writeln(now.toLocaleString() + "</br></br>");
-output();
+//var now = new Date();
+//document.writeln(now.toLocaleString() + "</br></br>");
+//output();
