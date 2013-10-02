ad688c55291001f295440727ef988b05ab6053f7
Raskolnikow <s.raskolnikow@gmail.com>
2013-10-02 16:47:40 +0200

    function closures & prototype proterty 3 column desigen + console output

--- School/script.js
+++ School/script.js
@@ -1,13 +1,40 @@
+var queue = new Array(10);
+
+var uniID = (function() {
+	var id = 0;
+	return function() { return id++; };
+})();
+
+for (var counter = 0; counter < 10; counter++) {
+	queue[counter] = uniID();
+};
+
 function output() {
 	var content = document.getElementById("content");
+
+	queue.shift();
+	queue.push(uniID());
+
+	content.innerHTML = "";
 	for (var i = 0; i < 10; i++) {
-		content.innerHTML += "Number: " + i + "</br>";
+		content.innerHTML += "d:\\users\\seba>" + "    0x" + queue[i].toString(16) + "</br>";
 	};
+	
 }
 
 function ss() {
-	output();
+	window.setInterval(output, 200);
 }
+
+
+
+
+// document.writeln("id: " + uniID() + "</br>");
+// document.writeln("id: " + uniID() + "</br>");
+// document.writeln("id: " + uniID() + "</br>");
+
+
+
 //var s = new String("Hello World from JavaScript");
 //document.writeln(s + "</br></br>");
 
