614072bc50d5bb36a61d180d5ec2f3f396223294
Sebastian Raskolnikow <s.raskolnikow@gmail.com>
2013-10-02 20:20:16 +0200

    New Buttons for changing base added

--- School/script.js
+++ School/script.js
@@ -1,4 +1,5 @@
 var queue = new Array(10);
+var base = 16;
 
 var uniID = (function() {
 	var id = 0;
@@ -17,13 +18,47 @@ function output() {
 
 	content.innerHTML = "";
 	for (var i = 0; i < 10; i++) {
-		content.innerHTML += "d:\\users\\seba>" + "    0x" + queue[i].toString(16) + "</br>";
+		switch (base) {
+			case 2:
+				content.innerHTML += "Bin:" + queue[i].toString(base) + "</br>";
+			break;
+
+			case 8:
+				content.innerHTML += "Oct:" + "0" + queue[i].toString(base) + "</br>";
+			break;
+
+			case 10:
+				content.innerHTML += "Dec:" + queue[i].toString(base) + "</br>";
+			break;
+			
+			case 16:
+				content.innerHTML += "Hex:" + "0x" + queue[i].toString(base) + "</br>";
+			break;
+
+			default: break;
+		}
 	};
 	
 }
 
+function btnBinary() {
+	base = 2;
+}
+
+function btnHex() {
+	base = 16;
+}
+
+function btnOctal() {
+	base = 8;
+}
+
+function btnDecimal() {
+	base = 10;
+}
+
 function ss() {
-	window.setInterval(output, 200);
+	window.setInterval(output, 500);
 }
 
 
