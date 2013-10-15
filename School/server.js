4de30bb5a1dbd0eb2c9d432d41a56358c1ba03e6
Sebastian Raskolnikow <s.raskolnikow@gmail.com>
2013-10-15 21:34:16 +0200

    connect static pages and body parsing

--- School/server.js
+++ School/server.js
@@ -4,9 +4,16 @@ var helloWorld = require('./hello_world');
 var app = connect();
 
 // setup logger middleware
-app.use(connect.logger('short'));
+app.use(connect.logger(':method :req[content-type]'));
 // setup the static file server
-app.use(connect.static(__dirname + '/public'));
+//app.use(connect.static(__dirname + '/public'));
+// setup the query middleware
+//app.use(connect.query());
+app.use(connect.bodyParser());
+
+app.use(function(req, res) {
+	res.end(JSON.stringify(req.body));
+});
 
 app.use(helloWorld);
 
