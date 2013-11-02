1d7b2c721a4cde31338eeffb7c9dbeaaecd3ae4b
Sebastian Raskolnikow <s.raskolnikow@gmail.com>
2013-11-02 17:19:28 +0100

    test

--- School/test_new_data.js
+++ School/test_new_data.js
@@ -10,14 +10,14 @@ var dbConfig = {
 var dao = new DataAccessObject(dbConfig);
 
 var now = new Date();
-now.setDate(1);
+//now.setDate(1);
 
 //console.log(now);
 
 //var date = d.toLocaleDateString();
 //var date = "Sunday, October 27, 2013";
 
-//dao.updateEntry(now, true);		// start
+dao.updateEntry(now, true);		// start
 //dao.updateEntry(now, false);		// stop
 
 dao.getEntry(now, function(data) {
